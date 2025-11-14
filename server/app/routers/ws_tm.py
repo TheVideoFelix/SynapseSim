8b2eea3a69d5dcb2bf3c55a487c583a9a57f2cbb
felix <54502652+TheVideoFelix@users.noreply.github.com>
2025-11-14 09:36:29 +0100

    feat(websocket): enhance WebSocket response with additional Turing machine state information and halt status

--- server/app/routers/ws_tm.py
+++ server/app/routers/ws_tm.py
@@ -23,17 +23,26 @@ async def websocket_tm_endpoint(websocket: WebSocket):
             if message_type == "init" and message_data is not None:
                 tm.input(message_data.get("input").split(','))
                 await websocket.send_json({
+                    "statesLen": len(tm.states),
+                    "alphabet": tm.alphabet,
+                    "tapeAlphabet": tm.tape_alphabet,
+                    "endState": tm.end_state,
+                    "startState": tm.start_state,
+                    "currentState": tm.state,
                     "tapes": [tape.get_tape_representation() for tape in tm.tapes],
-                    "lastMoveDirs": ['N'] * len(tm.tapes)
+                    "lastMoveDirs": ['N'] * len(tm.tapes),
+                    "isHalted": tm.is_halted
                 })
             elif message_type == "step":
-                directions = None
-                if not tm.state == tm.end_state:
-                    directions = [direction.value if hasattr(direction, 'value') else str(direction) for direction in tm.step()]
+                directions = tm.step()
+                if not tm.is_halted:
+                    directions = [direction.value if hasattr(direction, 'value') else str(direction) for direction in directions]
                     print(directions)
                 await websocket.send_json({
+                    "currentState": tm.state,
                     "tapes": [tape.get_tape_representation() for tape in tm.tapes],
-                    "lastMoveDirs": directions
+                    "lastMoveDirs": directions,
+                    "isHalted": tm.is_halted
                 })
             
     except WebSocketDisconnect:
